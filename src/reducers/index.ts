584de13f961e4e18548846ec8facef694bb5ea01
Tyler Hevia <tylerhevia@gmail.com>
2017-11-14 19:28:49 -0700

    finish setting up redux

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import test from './test';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  test
+});
 
 export default rootReducer;
