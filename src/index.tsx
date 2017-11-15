18f6c597816293ba07e3eb5e25db3aa2512d561d
Tyler Hevia <tylerhevia@gmail.com>
2017-11-14 19:08:34 -0700

    add root reducer, set up redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { Route, BrowserRouter } from 'react-router-dom';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+
+let initialState = {};
+
+const devtools: any = window['devToolsExtension']
+  ? window['devToolsExtension']()
+  : (f: any) => f;
+
+let middleware = applyMiddleware(thunk);
+const store: any = middleware(devtools(createStore))(rootReducer, initialState);
+
+const router = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>
 );
-registerServiceWorker();
+
+ReactDOM.render(router, document.getElementById('root'));
