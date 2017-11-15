70d38db6af8413f35625c54a54a9c9eb55c49cd1
Tyler Hevia <tylerhevia@gmail.com>
2017-11-14 19:44:31 -0700

    create Welcome component

---
+++ src/Welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1>TEAMS</h1>
+    </div>
+  );
+};
+
+export default Welcome;
