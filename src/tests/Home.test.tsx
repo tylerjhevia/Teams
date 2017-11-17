858c70942b952ed7e00908847eb6cdcfa382ba97
Tyler Hevia <tylerhevia@gmail.com>
2017-11-16 21:55:02 -0700

    fix type error with enzyme adapter

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from '../App';
+import Home from '../Home/Home';
 import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../App/App';
-import Navigation from '../Pages/Main/Navigation/Navigation';
+import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Home />, div);
 });
