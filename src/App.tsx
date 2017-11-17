0c0215144fa310efb0c5337b27bbf46504add5f8
Tyler Hevia <tylerhevia@gmail.com>
2017-11-16 21:37:46 -0700

    create Navigation component, add NavLinks to Home and Welcome pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Welcome from './Welcome/Welcome';
 import Home from './Home/Home';
+import Navigation from './Navigation/Navigation';
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <Route exact path="/" component={Welcome} />
         <Route exact path="/home" component={Home} />
       </div>
