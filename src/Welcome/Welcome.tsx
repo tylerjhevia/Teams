231d5fef73301099005fdfd6344ac03db11c4358
Tyler Hevia <tylerhevia@gmail.com>
2017-12-02 16:40:11 -0500

    add links for Login and Register pages to Welcome page

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -2,15 +2,31 @@ import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Welcome.css';
 
-const Welcome = () => {
-  return (
-    <div className="welcome-container">
-      <h1>TEAMS</h1>
-      <NavLink to="/home" className="start-button">
-        Build a team
-      </NavLink>
-    </div>
-  );
-};
+interface WelcomeState {
+  login: Boolean;
+  register: Boolean;
+}
 
-export default Welcome;
+export default class Welcome extends React.Component<{}, WelcomeState> {
+  constructor() {
+    super({});
+    this.state = {
+      login: false,
+      register: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="welcome-container">
+        <h1>TEAMS</h1>
+        <NavLink to="/login" className="login-link">
+          I have an account
+        </NavLink>
+        <NavLink to="/register" className="register-link">
+          I do not have an account
+        </NavLink>
+      </div>
+    );
+  }
+}
