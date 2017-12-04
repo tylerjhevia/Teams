4666757f79b88e9ecbb276df695a2472542d67e1
Tyler Hevia <tylerhevia@gmail.com>
2017-12-04 17:58:47 -0500

    style Teams component

--- src/Teams/Teams.tsx
+++ src/Teams/Teams.tsx
@@ -2,10 +2,28 @@ import * as React from 'react';
 import TeamsContainer from '../containers/TeamsContainer';
 import './Teams.css';
 
-const Teams = (props: Object) => {
+interface TeamsProps {
+  currentUser: CurrentUser;
+}
+
+interface CurrentUser {
+  id: Number;
+  username: String;
+  password: String;
+  created_at: String;
+  updated_at: String;
+}
+
+const Teams = (props: TeamsProps) => {
   return (
     <div className="teams-container">
+      <p className="current-user">
+        Logged in as: {props.currentUser.username}
+      </p>
       <h1>My teams</h1>
+      <ul className="teams-list">
+        <li>Team 1</li>
+      </ul>
     </div>
   );
 };
